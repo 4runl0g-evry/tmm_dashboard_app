d285a7812b06de84e4d0db8153193a89f8917333
Arun Loganathan <aloganathan@prendio.com>
2024-05-21 23:40:08 +0530

    chat emoji icon

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ page_title = "TEST MATURITY ASSESSMENT"
 process_type = ["QA PROCESS", "AUTOMATION TESTING","DATABASE TESTING",
                 "PERFORMANCE TESTING"]
 tab_titles = [ "DASHBOARD", "MATURITY ASSESSMENT",  
-              "PENDING AREAS / GAPS", "DATA PREVIEW", "CHAT with PDFs"]
+              "PENDING AREAS / GAPS", "DATA PREVIEW", "CHAT with PDFs:fire:"]
 maturity_level = {'MATURITY LEVEL': ['INITIAL (LEVEL 1)', 'MANAGED (LEVEL 2)', 
                                      'DEFINED (LEVEL 3)', 'MEASURED (LEVEL 4)',
                                      'OPTIMIZED (LEVEL 5)']}
